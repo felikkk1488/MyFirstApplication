63345804b5280f4b5224cfadc2d379333120af95
693-23 <gogoma228@gmail.com>
2026-01-17 13:52:53 +0300

    v1

---
+++ app/src/main/java/com/example/myfirstapplication_Sinitsyn_Maksumov/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.myfirstapplication_Sinitsyn_Maksumov
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            val randomNumber = Random.nextInt(1, 7)
+            textView.text = randomNumber.toString()
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
