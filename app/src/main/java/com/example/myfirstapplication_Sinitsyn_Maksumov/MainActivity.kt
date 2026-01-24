dce9dfbe0dfa44dc8b74c0519c4059ef222c1c6a
693-23 <gogoma228@gmail.com>
2026-01-24 14:24:00 +0300

    v2

--- app/src/main/java/com/example/myfirstapplication_Sinitsyn_Maksumov/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapplication_Sinitsyn_Maksumov/MainActivity.kt
@@ -1,29 +1,32 @@
 package com.example.myfirstapplication_Sinitsyn_Maksumov
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myfirstapplication_Sinitsyn_Maksumov.databinding.ActivityMainBinding
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
 
-        val textView = findViewById<TextView>(R.id.textView)
-        val button = findViewById<Button>(R.id.button)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        button.setOnClickListener {
+        binding.button.setOnClickListener {
             val randomNumber = Random.nextInt(1, 7)
-            textView.text = randomNumber.toString()
+            val resultText = getString(R.string.roll_result, randomNumber)
+            binding.textView.text = resultText
         }
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(view) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
